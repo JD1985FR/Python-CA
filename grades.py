0ebbf664d1b7d54b0463ab63faa8aabb50beb98c
JD1985FR <jimenez.girard.david@gmail.com>
2025-01-27 21:55:28 +0100

    grad

---
+++ grades.py
@@ -0,0 +1,12 @@
+grades = [90, 88, 62, 76, 74, 89, 48, 57]
+
+
+scaled_grades = [grade + 10 for grade in grades]
+print(scaled_grades)
+numbers = [2, -1, 79, 33, -45]
+doubled = []
+
+for number in numbers:
+  doubled.append(number * 2)
+
+print(doubled)
