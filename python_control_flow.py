9a0bd37c2d1c5e8c4ab6955728b0e7e7e9dd0a6c
JD1985FR <jimenez.girard.david@gmail.com>
2025-02-08 21:40:42 +0100

    Python_control_flow

--- python_control_flow.py
+++ python_control_flow.py
@@ -148,3 +148,44 @@ print(always_false(1))
 
 
 
+# Write your movie_review function here:
+
+def movie_review(rating):
+  if(rating <= 5):
+    return "Avoid at all costs!"
+  if(rating < 9):
+    return "This one was fun."
+  return "Outstanding!"
+
+# Uncomment these function calls to test your movie_review function:
+print(movie_review(9))
+# should print "Outstanding!"
+print(movie_review(4))
+# should print "Avoid at all costs!"
+print(movie_review(6))
+# should print "This one was fun."
+
+
+
+
+
+# Write your max_num function here:
+def max_num(num1, num2, num3):
+  if num1 > num2 and num1 > num3:
+    return num1
+  elif num2 > num1 and num2 > num3:
+    return num2
+  elif num3 > num1 and num3 > num2:
+    return num3
+  else:
+    return "It's a tie!"
+
+# Uncomment these function calls to test your max_num function:
+print(max_num(-10, 0, 10))
+# should print 10
+print(max_num(-10, 5, -30))
+# should print 5
+print(max_num(-5, -10, -10))
+# should print -5
+print(max_num(2, 3, 3))
+# should print "It's a tie!"
